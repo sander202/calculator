b22788c5579d2fc478b119b0597d0f8741033d90
Sander <sanderkuusik@hotmail.com>
2022-05-29 22:07:16 +0300

    wired up buttons with functions

--- calculator.js
+++ calculator.js
@@ -1,38 +1,61 @@
 let output = "";
-
+let chosenOperator = "";
+let storedNumber = "";
+let result = "";
 
 const add = (num1, num2) => {
-    return num1 + num2;
+    return JSON.parse(num1) + JSON.parse(num2);
 }
 
 const subtract = (num1, num2) => {
-    return num1 - num2;
+    return JSON.parse(num1) - JSON.parse(num2);
 }
 
 const multiply = (num1, num2) => {
-    return num1 * num2;
+    return JSON.parse(num1) * JSON.parse(num2);
 }
 
 const divide = (num1, num2) => {
-    return num1 / num2;
+    let division = JSON.parse(num1) / JSON.parse(num2);
+    return Math.round(division * 1000) / 1000
 }
 
 const operate = (operator, num1, num2) => {
+    const displayedNumber = document.querySelector('.display');
     if (operator === "+") {
-        return add(num1, num2);
+        result = add(num1, num2);
+        displayedNumber.textContent = result;
     } else if (operator === "-") {   
-        return subtract(num1, num2); 
+        result = subtract(num1, num2); 
+        displayedNumber.textContent = result;
     } else if (operator === "*") {   
-        return multiply(num1, num2);  
+        result = multiply(num1, num2); 
+        displayedNumber.textContent = result; 
     } else {
-        return divide(num1, num2);
-    
+        result = divide(num1, num2);
+        displayedNumber.textContent = result;
     }
 }
 
-const display = (number) => {
+const clearScreen = () => {
     const displayedNumber = document.querySelector('.display');
-    output += number;
+    displayedNumber.textContent = "0";
+    storedNumber = "";
+    output = "";
+    chosenOperator = "";
+}
+
+const selectOperator = (operator) => {
+    const displayedNumber = document.querySelector('.display');
+    displayedNumber.textContent = operator;
+    chosenOperator = operator;
+    storedNumber = output;
+    output = "";
+}
+
+const display = (num) => {
+    const displayedNumber = document.querySelector('.display');
+    output += num;
     displayedNumber.textContent = output;
-    let storedNumber = output;
+    
 }
