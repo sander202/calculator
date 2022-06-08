c3fe228eb105fb3c22919a96a677457933e673e6
Sander <sanderkuusik@hotmail.com>
2022-06-08 20:50:45 +0300

    improved functionality, decimals

--- calculator.js
+++ calculator.js
@@ -1,61 +1,100 @@
-let output = "";
-let chosenOperator = "";
+let displayedNumber = "";
+let chosenOperator = [];
 let storedNumber = "";
 let result = "";
+let index = 0;
 
 const add = (num1, num2) => {
-    return JSON.parse(num1) + JSON.parse(num2);
+    let addition = JSON.parse(num1) + JSON.parse(num2);
+    return Math.round(addition * 1000) / 1000
 }
 
 const subtract = (num1, num2) => {
-    return JSON.parse(num1) - JSON.parse(num2);
+    let subtraction = JSON.parse(num1) - JSON.parse(num2);
+    return Math.round(subtraction * 1000) / 1000
 }
 
 const multiply = (num1, num2) => {
-    return JSON.parse(num1) * JSON.parse(num2);
+    let multiplication = JSON.parse(num1) * JSON.parse(num2);
+    return Math.round(multiplication * 1000) / 1000
 }
 
 const divide = (num1, num2) => {
-    let division = JSON.parse(num1) / JSON.parse(num2);
-    return Math.round(division * 1000) / 1000
-}
-
-const operate = (operator, num1, num2) => {
-    const displayedNumber = document.querySelector('.display');
-    if (operator === "+") {
-        result = add(num1, num2);
-        displayedNumber.textContent = result;
-    } else if (operator === "-") {   
-        result = subtract(num1, num2); 
-        displayedNumber.textContent = result;
-    } else if (operator === "*") {   
-        result = multiply(num1, num2); 
-        displayedNumber.textContent = result; 
+    if (num2 === "0") {
+        storedNumber = "";
+        displayedNumber = "";
+        chosenOperator = [];
+        index = 0;
+        return "Nope";
     } else {
-        result = divide(num1, num2);
-        displayedNumber.textContent = result;
+        let division = JSON.parse(num1) / JSON.parse(num2);
+        return Math.round(division * 1000) / 1000
     }
 }
 
-const clearScreen = () => {
-    const displayedNumber = document.querySelector('.display');
-    displayedNumber.textContent = "0";
+const operate = (operator, num1, num2) => {
+    const screen = document.querySelector('.display');
+    if (!num1 || !num2) {
+        screen.textContent = displayedNumber || storedNumber;
+    } else {
+        if (operator === "+") {
+            result = add(num1, num2);   
+        } else if (operator === "-") {   
+            result = subtract(num1, num2); 
+        } else if (operator === "*") {   
+            result = multiply(num1, num2); 
+        } else {
+            result = divide(num1, num2);
+        }
+        if (result !== "Nope") {
+            storedNumber = result;
+        } 
+        screen.textContent = result;
+        displayedNumber = "";
+        return result;
+    }    
+}
+
+const clearAll = () => {
+    const screen = document.querySelector('.display');
+    screen.textContent = "0";
     storedNumber = "";
-    output = "";
-    chosenOperator = "";
+    displayedNumber = "";
+    chosenOperator = [];
+    index = 0;
+}
+
+const decimal = (point) => {
+    const screen = document.querySelector('.display');
+    if (!screen.textContent.includes(point)) {
+        if (screen.textContent === "0") {
+            displayedNumber = "0";
+        }
+        displayedNumber += point;
+        screen.textContent = displayedNumber; 
+    }  
 }
 
 const selectOperator = (operator) => {
-    const displayedNumber = document.querySelector('.display');
-    displayedNumber.textContent = operator;
-    chosenOperator = operator;
-    storedNumber = output;
-    output = "";
+    chosenOperator[index] = operator;
+    if (storedNumber != "" && displayedNumber != "") {
+        operate(chosenOperator[index - 1], storedNumber, displayedNumber);
+    } else if (storedNumber != "") {      
+        index++;
+        return;
+    } else {
+        storedNumber = displayedNumber;
+        displayedNumber = "";
+    }
+    index++;
 }
 
-const display = (num) => {
-    const displayedNumber = document.querySelector('.display');
-    output += num;
-    displayedNumber.textContent = output;
-    
+const display = (digit) => {
+    const screen = document.querySelector('.display');
+    if (screen.textContent === "0") {
+        displayedNumber = digit;
+    } else {
+        displayedNumber += digit;      
+    }
+    screen.textContent = displayedNumber;  
 }
